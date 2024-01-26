2d7d4d34eb67a1d258d49bf25591f1b89579720e
EthanGardner18 <ethangrdnr@gmail.com>
2024-01-26 13:37:19 -0600

    branch works

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,8 @@ name = input("What is your name?")
 age = (int)(input("What is your age?"))
 for y in range(0,age,1):
     print(name)
+
+
+email = input("What is your email?")
+for y in range(0,age,1):
+    print(email)
